47d8cccde80d7dcd9646c61a4f5526b4b3f9844b
hp22000150 <hp22000150@gmail.com>
2026-06-20 18:22:26 +0900

    Add PWA support: service worker, manifests for PC and mobile

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'workdaily-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/mobile.html',
+  '/report.html',
+  '/flatpickr.min.css',
+  '/flatpickr.min.js',
+  '/icon.svg',
+  '/manifest.json',
+  '/manifest-pc.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
